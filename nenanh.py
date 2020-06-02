394eb02aa4eee8bf5f4ba230acbb56cfca43f6f6
hungking98 <hungking98@gmail.com>
2020-06-02 18:10:33 +0700

    change

--- nenanh.py
+++ nenanh.py
@@ -5,20 +5,24 @@ from sklearn.cluster import KMeans
 import sys
 from numpy.ma.core import size
 
+#đọc ảnh vào bằng 1 hàm. Hàm này cho ra img là một ma trận số. Mỗi phần tử của ma trận là một pixel
 img = mpimg.imread('girl3.jpg')
+#tách ma trận số thành một mảng các vec tơ , mỗi vecto sẽ có 3 chiều với ảnh màu
 X = img.reshape((img.shape[0]*img.shape[1], img.shape[2]))
 
-#nén 
+#Bước này là xây dựng codeBook. Dùng 1 hàm phân cụm nào đó để tìm ra các codeVecto. ở đây mình dùng thuật toán Kmean
 kmeans = KMeans(n_clusters=10).fit(X)
 label = kmeans.predict(X)
-print(label)
 img4 = np.zeros_like(X)
 
-    # replace each pixel by its center
+#Chỗ này là thay thế những mảng vec tơ ban đầu thành mảng các codebook tương ứng. Vec tơ thuộc cụm nào, gần vecto codebook nào nhất
+#thì thay bằng code vecto đó
+
+#Này là codebook có 10 vecto    
 for k in range(10):
     img4[label == k] = kmeans.cluster_centers_[k]
-    # reshape and display output image
 
+#Đảo ngược lại mảng để trở về ma trận ảnh số
 img5 = img4.reshape((img.shape[0], img.shape[1], img.shape[2]))
 plt.imshow(img5)
 
